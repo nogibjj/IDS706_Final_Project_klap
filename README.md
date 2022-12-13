aae77d22fb8a281f3c86f7793fa21283d5b2b155
kashafali8 <111402572+kashafali8@users.noreply.github.com>
2022-12-13 13:43:50 +0000

    hf_dataset_query for click function

--- README.md
+++ README.md
@@ -6,4 +6,7 @@
 
 ## HuggingFace Dataset ETL (Extraction, Transformation, Loading):
 1. Build a search tool for looking for datasets
-2. Loaded two datasets from HuggingFace: 
+2. Build a datset inspection function
+3. Click function for datasets search in hf_dataset_query.py: chmod +x hf_dataset_query.py
+4. Add color to the click function results
+2. Loaded two datasets from HuggingFace
--- hlib/loader.py
+++ hlib/loader.py
@@ -1,8 +1,27 @@
-from datasets import load_dataset
+from datasets import list_datasets, load_dataset
+
+# build a dataset search query
+def search(query, limit=10, sort=True):
+    """Search for a dataset by name or description"""
+
+    # get all datasets that match the query
+    datasets = [dataset for dataset in list_datasets() if query in dataset]
+    sorted_datasets = sorted(datasets) if sort else datasets
+    limited_result = sorted_datasets if limit is None else sorted_datasets[:limit]
+    return limited_result
+
+# inspect a dataset
+def inspect_data(dataset, column="train", rows=5):
+    """Inspect a dataset"""
+
+    # load dataset
+    dataset = load_dataset(dataset)
+    # get first 5 rows of first split
+    data = dataset[column][0:rows]
+    return data
 
 # load rotten tomatoes dataset
-rotten_tomatoes = load_dataset("rotten_tomatoes")
+# rotten_tomatoes = load_dataset("rotten_tomatoes")
 
 # print the first row in the training set for the rotten tomatoes dataset
-print(rotten_tomatoes["train"][0])
-
+# print(rotten_tomatoes["train"][0])
