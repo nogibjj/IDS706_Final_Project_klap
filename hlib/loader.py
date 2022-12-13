f56f04c9c379aa762b256822b4532b75bf5092ac
kashafali8 <111402572+kashafali8@users.noreply.github.com>
2022-12-13 12:49:22 +0000

    make format

--- hlib/loader.py
+++ hlib/loader.py
@@ -1,8 +1,7 @@
 from datasets import load_dataset
 
-#load rotten tomatoes dataset
+# load rotten tomatoes dataset
 rotten_tomatoes = load_dataset("rotten_tomatoes")
 
-#print the first row in the training set for the rotten tomatoes dataset
+# print the first row in the training set for the rotten tomatoes dataset
 print(rotten_tomatoes["train"][0])
-
