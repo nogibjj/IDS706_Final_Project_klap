02cf0a3f41200bb0998778deaa35963d12005d73
kashafali8 <111402572+kashafali8@users.noreply.github.com>
2022-12-14 23:52:58 +0000

    cloned repo

---
+++ pull_data.py
@@ -0,0 +1,18 @@
+import pandas as pd
+#import generate_reviews fil from reviews and src directory
+import reviews.src.generate_reviews
+from reviews.src import generate_reviews
+
+
+# using pandas to read the csv file
+#df = pd.read_csv('combined_movie_reviews.csv')
+
+#df.head()
+# df = gr.generate("imdb", 10)
+# print(df)
+# # # create a function that randomly generates sample of 1000 rows
+# def sample_generator():
+#     df_sample = df.sample(n=1000)
+#     return df_sample
+
+# sample_generator()
