862d18956a790b8c7b322b52c6ec287958d338bc
kashafali8 <111402572+kashafali8@users.noreply.github.com>
2022-12-15 23:51:41 +0000

    new fastapi to link with s3

---
+++ fast_api.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+import uvicorn
+from pull_data import sample_generator
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+
+app = FastAPI()
+
+# end point 1 : welcome message
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the Movie Review Application"}
+
+# end point 2 : list a random row of the dataset from sample_generator
+@app.get("/movie_reviews/{source}/{size}")
+async def source(source:str, size:int):
+    movie_reviews = sample_generator(source,size)
+    return {"movie_reviews":movie_reviews}
+
+# save response to a file
+@app.get("/save/{review}")
+async def save(review:str):
+    file = open("review.txt", "w")
+    file.write(review)
+    file.close()
+    return {"review":review}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8080, host="0.0.0.0")
